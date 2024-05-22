024b4da882c685341686a0939fe152d31bb7da2f
AxelBresa <bresanovichaxel43@gmail.com>
2024-05-22 11:40:16 -0300

    Se agrego la ruta correcta del guardado de img

--- ejercio2(img)/scraping(img).py
+++ ejercio2(img)/scraping(img).py
@@ -1,5 +1,8 @@
+import os
 import requests
 from bs4 import BeautifulSoup
+
+os.makedirs("./ejercio2(img)/imagenes", exist_ok=True)
 
 def getdata(url):
     r = requests.get(url)
@@ -7,11 +10,24 @@ def getdata(url):
 
 htmldata = getdata("https://www.geeksforgeeks.org/")
 
-print("img")
 soup = BeautifulSoup(htmldata, 'html.parser')
+
+def download_image(url, folder="./ejercio2(img)/imagenes"):
+    try:
+        response = requests.get(url, stream=True)
+        # Extraer el nombre del archivo de la URL
+        filename = os.path.join(folder, url.split("/")[-1])
+        # Guardar la imagen en la carpeta
+        with open(filename, 'wb') as file:
+            for i in response:
+                file.write(i)
+        print(f"Imagen descargada: {filename}")
+    except Exception as e:
+        print(f"No se pudo descargar {url}. Error: {e}")
 
 valid_extensions = ['.png', '.jpg', '.jpeg', '.webp']
 for item in soup.find_all('img'):
     src = item.get('src', '')
     if any(src.lower().endswith(ext) for ext in valid_extensions):
-        print(src)
+        download_image(src)
+
