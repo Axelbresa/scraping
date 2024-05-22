310125e0bfea55401e16c1c564205cc328182dca
AxelBresa <bresanovichaxel43@gmail.com>
2024-05-22 12:35:17 -0300

    Se agrego comentarios

--- ejercio2(img)/modulos.py
+++ ejercio2(img)/modulos.py
@@ -1,26 +1,31 @@
+# Importamos los paquetes
 import os
 import requests
 from bs4 import BeautifulSoup
 
+# Creamos una carperta imagenes
 os.makedirs("./ejercio2(img)/imagenes", exist_ok=True)
 
+# Hace una solicitud GET a la URL y devuelve el contenido en formato de texto (HTML).
 def getdata(url):
     r = requests.get(url)
     return r.text
 
+# Parseamos el HTML y en soup se guarda la estructura del HTML.
 def parsearHtml():
     htmldata = getdata("https://www.geeksforgeeks.org/")
     soup = BeautifulSoup(htmldata, 'html.parser')
     valid_extensions = ['.png', '.jpg', '.jpeg', '.webp']
     img_extension(soup, valid_extensions)
 
-
+# Valimos si las extensiones de la img es jpg, png, jpeg o webp
 def img_extension(soup, valid_extensions):
     for item in soup.find_all('img'):
         src = item.get('src', '')
         if any(src.lower().endswith(ext) for ext in valid_extensions):
             download_image(src)
 
+# Las images lo guardamos  en la carpeta imagenes
 def download_image(url, folder="./ejercio2(img)/imagenes"):
     try:
         response = requests.get(url, stream=True)
@@ -34,4 +39,5 @@ def download_image(url, folder="./ejercio2(img)/imagenes"):
     except Exception as e:
         print(f"No se pudo descargar {url}. Error: {e}")
 
+# Llamamos a la funcion
 parsearHtml()
